acc7a46fdf6f205f1b2ecb870e4aed3b09d21fe7
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-02-26 10:14:16 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, createBrowserRouter } from "react-router-dom"
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
-
 
 const router = createBrowserRouter([
   {
