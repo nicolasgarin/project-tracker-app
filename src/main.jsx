f02b0218989c1779901a21bad07219f2d1494ed1
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-02-26 11:10:23 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "/projects/",
+        path: "/project-tracker-app/projects/",
         element: <Project />,
       },
     ],
