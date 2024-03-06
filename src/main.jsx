f4dcff1ec117519ff7101603981fcc76da2a7171
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-03-06 08:46:57 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { DataProvider } from "./context/DataContext";
+import { UserOptionsProvider } from './context/UserOptionsContext.jsx';
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -11,10 +12,12 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <DataProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </DataProvider>
+    <UserOptionsProvider>
+      <DataProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </DataProvider>
+    </UserOptionsProvider>
   </React.StrictMode>
 );
