5ed3f7157f9639eb86fe3f3d4b20c5b585fb2ea5
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-03-06 08:54:56 -0300

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import logo from '../../assets/logo-celeste-solo-circulo.svg'
+import { useUserOptions } from '../context/UserOptionsContext';
+import { IoMoon, IoSunny } from "react-icons/io5";
 
 export default function Header() {
+    const { theme, toggleTheme } = useUserOptions()
+    
     return (
         <header className='header'>
             <div className='container d-flex align-items-center'>
-            <img src={logo} className='logo' alt='project tracker logo' />
+                <img src={logo} className='logo' alt='project tracker logo' />
                 <h1 className='titulo-header'>Project Tracker</h1>
+                <div className='switch-container'>
+                    <IoMoon />
+                    <label className="switch">
+                    <input type='checkbox' checked={theme === "light"} onChange={toggleTheme} />
+                    <span className="slider round" />
+                    </label>
+                    <IoSunny />
+                </div>
             </div>
         </header>
     )
