eb3de35961b233dcf9385773ee087755a0e1e2b0
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-03-06 08:59:53 -0300

    Update Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useUserOptions } from '../context/UserOptionsContext'
 
 export default function Footer() {
+    const { theme, toggleTheme } = useUserOptions()
     return (
-        <footer>
+        <footer className={theme}>
             <div className='container d-flex justify-content-end'>
                 <div className='texto-footer'>Desarrollado por Nicolás Garín</div>
             </div>
