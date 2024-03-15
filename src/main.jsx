0baa976c38dc049e09a9c230f1bed4143cdc49c7
nicolasgarin <88353332+nicolasgarin@users.noreply.github.com>
2024-03-15 09:34:12 -0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { DataProvider } from "./context/DataContext";
 import { UserOptionsProvider } from './context/UserOptionsContext.jsx';
 
@@ -10,24 +10,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-const router = createBrowserRouter([
-  {
-    path: "/project-tracker-app/",
-    element: <App />,
-    children: [
-      {
-        path: "/project-tracker-app/",
-        element: <Home />,
-      },
-    ],
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserOptionsProvider>
       <DataProvider>
-      <RouterProvider router={router} />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </DataProvider>
     </UserOptionsProvider>
   </React.StrictMode>
